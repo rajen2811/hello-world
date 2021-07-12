40fe49d81d2f3a5cad484510d5f611b2cb91424f
rashmi <ms.rashmi.pati@gmail.com>
2021-07-12 18:40:49 +0530

    initializing the repo

---
+++ hello.py
@@ -0,0 +1,12 @@
+# This is the sample hello python print script
+def print_hello():
+    print "Hello World"
+    return "Hello World"
+
+
+def main():
+    print_hello()
+
+
+if __name__ == '__main__':
+    main()
